e9bae870b7f8a5adb4c6ca184f3d2981ec7b38ba
zainuddin25 <zanuk12345>
2023-05-03 16:07:55 +0700

    fix sintax

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ app.use(express.json())
 
 app.use('/users', userRoutes)
 
+app.use('/', (req, res) => {
+    res.send('Hallo World')
+})
 
 app.listen(3222, () => {
     console.log("Server Started In Port 3222")
